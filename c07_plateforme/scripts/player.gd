f58c9281176196c1e508b24e12bbbeb2c2abcbf5
MaiveCL <maive_@hotmail.com>
2025-10-13 19:38:04 -0400

    AnimationTree done, mais bug un peu

--- c07_plateforme/scripts/player.gd
+++ c07_plateforme/scripts/player.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
+const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+#@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var anim_tree: AnimationTree = $AnimationTree
+@onready var state_machine: AnimationNodeStateMachinePlayback = anim_tree.get("parameters/playback")
+
+func _ready():
+	anim_tree.active = true  # Active l'AnimationTree
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -16,18 +20,17 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 		
 	if Input.is_action_just_pressed("attack"):
-		animated_sprite.play("attack")
-
+		state_machine.travel("attack")  # On passe à l'état attack
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
-		animated_sprite.play("run")
+		state_machine.travel("run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animated_sprite.play("idle")
+		state_machine.travel("idle")
 
-	animated_sprite.flip_h = direction < 0
+	$SpriteSheet.flip_h = direction < 0
 
 	move_and_slide()
