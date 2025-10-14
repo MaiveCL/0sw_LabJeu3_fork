abc9f04a5ce6175fb31d7f68d6581134019f7913
MaiveCL <maive_@hotmail.com>
2025-10-13 20:28:03 -0400

    state machine done

--- c07_plateforme/scripts/player.gd
+++ c07_plateforme/scripts/player.gd
@@ -6,6 +6,7 @@ const JUMP_VELOCITY = -400.0
 #@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var anim_tree: AnimationTree = $AnimationTree
 @onready var state_machine: AnimationNodeStateMachinePlayback = anim_tree.get("parameters/playback")
+var current_state: String = "idle"  # garde en mémoire l'état actuel
 
 func _ready():
 	anim_tree.active = true  # Active l'AnimationTree
@@ -17,20 +18,33 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
+		print("jump pressed !")
 		velocity.y = JUMP_VELOCITY
 		
 	if Input.is_action_just_pressed("attack"):
-		state_machine.travel("attack")  # On passe à l'état attack
+		print("Attack pressed !")
+		change_state("attack")  # On passe à l'état attack
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
-		state_machine.travel("run")
+		change_state("run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		state_machine.travel("idle")
+		if current_state != "attack":
+			change_state("idle")
 
 	$SpriteSheet.flip_h = direction < 0
 
 	move_and_slide()
+	
+func change_state(new_state: String) -> void:
+	if current_state != new_state:
+		current_state = new_state
+		state_machine.travel(new_state)
+		print("State changed to: ", new_state)
+		
+	# relayer le state machine à l'animation Tree quand en state attack
+	if current_state == "attack":
+		current_state = state_machine.get_current_node()
